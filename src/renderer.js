96c0c50fb0a8273fde9585575b3e71eb729be4e1
scarlat-trainer <78.cristi@gmail.com>
2024-03-24 19:07:09 +0200

    add functionallity to download button, add download success message

--- src/renderer.js
+++ src/renderer.js
@@ -1,8 +1,10 @@
 const getVideoBtn = document.querySelector(".input-container button");
+const downloadBtn = document.querySelector(".download-button");
 const getVideoInput = document.querySelector(".input-container input");
 const videoTitle = document.querySelector(".video-title");
 const videoInfo = document.querySelector(".video-info");
 const player = document.querySelector(".players-container iframe");
+const playerContainer = document.querySelector(".players-container");
 const progressAudio = document.querySelector("#progress-audio .progress-bar-fill");
 const progressVideo = document.querySelector("#progress-video .progress-bar-fill");
 const finishMsg = document.querySelector(".dl-finish-info");
@@ -17,8 +19,27 @@ window.onload = () => {
     }
   }
 
+  downloadBtn.onclick = () => {
+    if(getVideoInput.value && getVideoInput.value !== ''){
+      window.electronAPI.getVideo({url: getVideoInput.value, type: 'download-video'})
+    }
+    else {
+      console.log("Please paste url in input.")
+    }
+  }
+
+  getVideoInput.onchange = () => {
+    videoTitle.innerText = "";
+    videoInfo.innerText = "";
+    finishMsg.style.display = "none";
+    playerContainer.style.display = "none";
+  }
+
   window.electronAPI.onMsgToRenderer((data) => {
     if(data.type === "video-info"){
+      videoTitle.style.display = "block";
+      videoInfo.style.display = "block";
+      playerContainer.style.display = "block";
       videoTitle.innerText = data.payload.title;
       videoInfo.innerText = data.payload.details.description;
       player.src = data.payload.details.embed.iframeUrl;
@@ -32,7 +53,8 @@ window.onload = () => {
       progressVideo.style.width = `${data.payload.progress.video}%`
     }
     else if(data.type === "video-dl-finish"){
-      finishMsg.innerText = `Download finish, you can find file to ${data.payload.videoPath}.`
+      finishMsg.style.display = "block";
+      finishMsg.innerText = `Download finish, your file is ${data.payload.videoPath}.`
     }
   });
 }
--- src/yt-dl-service.js
+++ src/yt-dl-service.js
@@ -59,110 +59,112 @@ const youtubeDownloadHQ = async (videoUrl, type, donloadsDirPath, cb) => {
     console.log(">>>>", e)
   }
   if(type === 'only-info')return
-  const ref = videoUrl;
-  const tracker = {
-    start: Date.now(),
-    audio: {downloaded: 0, total: Infinity},
-    video: {downloaded: 0, total: Infinity},
-    merged: {frame: 0, speed: '0x', fps: 0},
-  };
+  else if(type === 'download-video'){
+    const ref = videoUrl;
+    const tracker = {
+      start: Date.now(),
+      audio: {downloaded: 0, total: Infinity},
+      video: {downloaded: 0, total: Infinity},
+      merged: {frame: 0, speed: '0x', fps: 0},
+    };
 
 // Get audio and video streams
-  const audio = ytdl(ref, {quality: 'highestaudio'})
-    .on('progress', (_, downloaded, total) => {
-      tracker.audio = {downloaded, total};
-    });
-  const video = ytdl(ref, {quality: 'highestvideo'})
-    .on('progress', (_, downloaded, total) => {
-      tracker.video = {downloaded, total};
-    });
+    const audio = ytdl(ref, {quality: 'highestaudio'})
+      .on('progress', (_, downloaded, total) => {
+        tracker.audio = {downloaded, total};
+      });
+    const video = ytdl(ref, {quality: 'highestvideo'})
+      .on('progress', (_, downloaded, total) => {
+        tracker.video = {downloaded, total};
+      });
 
 // Prepare the progress bar
-  let progressbarHandle = null;
-  const progressbarInterval = 1000;
-  const showProgress = () => {
-    readline.cursorTo(process.stdout, 0);
-    const toMB = i => (i / 1024 / 1024).toFixed(2);
+    let progressbarHandle = null;
+    const progressbarInterval = 1000;
+    const showProgress = () => {
+      readline.cursorTo(process.stdout, 0);
+      const toMB = i => (i / 1024 / 1024).toFixed(2);
 
-    process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-    process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+      process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+      process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
 
-    process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-    process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+      process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+      process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
 
-    process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-    process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+      process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+      process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
 
-    process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+      process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
 
-    cb({
-      type: "video-dl-progress",
-      payload: {
-        progress: {
-          audio: (tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2),
-          video: (tracker.video.downloaded / tracker.video.total * 100).toFixed(2),
-          runningFor: ((Date.now() - tracker.start) / 1000 / 60).toFixed(2)
+      cb({
+        type: "video-dl-progress",
+        payload: {
+          progress: {
+            audio: (tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2),
+            video: (tracker.video.downloaded / tracker.video.total * 100).toFixed(2),
+            runningFor: ((Date.now() - tracker.start) / 1000 / 60).toFixed(2)
+          }
         }
-      }
-    })
-    readline.moveCursor(process.stdout, 0, -3);
-  };
+      })
+      readline.moveCursor(process.stdout, 0, -3);
+    };
 
 // Start the ffmpeg child process
-  const ffmpegProcess = cp.spawn(ffmpeg, [
-    // Remove ffmpeg's console spamming
-    '-loglevel', '8', '-hide_banner',
-    // Redirect/Enable progress messages
-    '-progress', 'pipe:3',
-    // Set inputs
-    '-i', 'pipe:4',
-    '-i', 'pipe:5',
-    // Map audio & video from streams
-    '-map', '0:a',
-    '-map', '1:v',
-    // Keep encoding
-    '-c:v', 'copy',
-    // Define output file
-    //'out.mp4'
-    `${donloadsDirPath}/${data.data[0].title}.mp4`,
-  ], {
-    windowsHide: true,
-    stdio: [
-      /* Standard: stdin, stdout, stderr */
-      'inherit', 'inherit', 'inherit',
-      /* Custom: pipe:3, pipe:4, pipe:5 */
-      'pipe', 'pipe', 'pipe',
-    ],
-  });
-  ffmpegProcess.on('close', () => {
-    // Cleanup
-    clearInterval(progressbarHandle);
-    console.clear();
-    console.log('done');
-    cb({
-      type: "video-dl-finish",
-      payload: {
-        videoPath: `${donloadsDirPath}/${data.data[0].title}.mp4`,
-      }
-    })
-  });
+    const ffmpegProcess = cp.spawn(ffmpeg, [
+      // Remove ffmpeg's console spamming
+      '-loglevel', '8', '-hide_banner',
+      // Redirect/Enable progress messages
+      '-progress', 'pipe:3',
+      // Set inputs
+      '-i', 'pipe:4',
+      '-i', 'pipe:5',
+      // Map audio & video from streams
+      '-map', '0:a',
+      '-map', '1:v',
+      // Keep encoding
+      '-c:v', 'copy',
+      // Define output file
+      //'out.mp4'
+      `${donloadsDirPath}/${data.data[0].title}.mp4`,
+    ], {
+      windowsHide: true,
+      stdio: [
+        /* Standard: stdin, stdout, stderr */
+        'inherit', 'inherit', 'inherit',
+        /* Custom: pipe:3, pipe:4, pipe:5 */
+        'pipe', 'pipe', 'pipe',
+      ],
+    });
+    ffmpegProcess.on('close', () => {
+      // Cleanup
+      clearInterval(progressbarHandle);
+      console.clear();
+      console.log('done');
+      cb({
+        type: "video-dl-finish",
+        payload: {
+          videoPath: `${donloadsDirPath}/${data.data[0].title}.mp4`,
+        }
+      })
+    });
 
 // Link streams
 // FFmpeg creates the transformer streams and we just have to insert / read data
-  ffmpegProcess.stdio[3].on('data', chunk => {
-    // Start the progress bar
-    if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
-    // Parse the param=value list returned by ffmpeg
-    const lines = chunk.toString().trim().split('\n');
-    const args = {};
-    for (const l of lines) {
-      const [key, value] = l.split('=');
-      args[key.trim()] = value.trim();
-    }
-    tracker.merged = args;
-  });
-  audio.pipe(ffmpegProcess.stdio[4]);
-  video.pipe(ffmpegProcess.stdio[5]);
+    ffmpegProcess.stdio[3].on('data', chunk => {
+      // Start the progress bar
+      if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+      // Parse the param=value list returned by ffmpeg
+      const lines = chunk.toString().trim().split('\n');
+      const args = {};
+      for (const l of lines) {
+        const [key, value] = l.split('=');
+        args[key.trim()] = value.trim();
+      }
+      tracker.merged = args;
+    });
+    audio.pipe(ffmpegProcess.stdio[4]);
+    video.pipe(ffmpegProcess.stdio[5]);
+  }
 }
 
 module.exports = {getYoutubeDownloadableUrls, youtubeDownloadHQ}
